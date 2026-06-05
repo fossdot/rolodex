45b7df336b6f90a20207f74abbfe9ce224c25f83
Vishal Arya <vishal@bodhya.net>
2026-06-05 11:30:35 +0530

    Mobile: Add Contact button is icon-only

--- frontend/src/lib/components/ContactsBrowser.svelte
+++ frontend/src/lib/components/ContactsBrowser.svelte
@@ -125,7 +125,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
         <path d="M5 12h14M12 5v14"/>
       </svg>
-      Add<span class="hidden sm:inline">&nbsp;Contact</span>
+      <span class="hidden sm:inline">Add Contact</span>
     </a>
   </div>
 
